8cd697882359ec52234bb4443ccbd6169549709c
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-09-27 10:05:02 -0400

    move hierarchy reading into its own class

--- src/lib.rs
+++ src/lib.rs
@@ -240,10 +240,14 @@ impl From<lz4_flex::block::DecompressError> for ReaderError {
 
 type Result<T> = std::result::Result<T, ReaderError>;
 
-struct Reader<R: Read + Seek> {
+struct HeaderReader<R: Read + Seek> {
     input: R,
     header: Option<Header>,
     buf: [u8; 128], // used for reading
+}
+
+struct HierarchyReader<R: Read> {
+    input: R,
 }
 
 #[inline]
@@ -265,6 +269,7 @@ fn get_variant_32(bytes: &[u8]) -> Result<(u32, usize)> {
     Ok((res, len))
 }
 
+#[inline]
 fn read_variant_32(input: &mut impl Read) -> Result<u32> {
     let mut res = 0u32;
     for ii in 0..8 {
@@ -303,70 +308,88 @@ fn read_c_str(input: &mut impl Read, max_len: usize) -> Result<String> {
 const HIERARCHY_NAME_MAX_SIZE: usize = 512;
 const HIERARCHY_ATTRIBUTE_MAX_SIZE: usize = 65536 + 4096;
 
-fn read_hierarchy_entry(hierarchy: &mut impl Read) -> Result<HierarchyEntry> {
-    let entry_tpe = read_u8(hierarchy)?;
-    let entry = match entry_tpe {
-        254 => {
-            // VcdScope (ScopeType)
-            let tpe = ScopeType::try_from_primitive(read_u8(hierarchy)?)?;
-            let name = read_c_str(hierarchy, HIERARCHY_NAME_MAX_SIZE)?;
-            let component = read_c_str(hierarchy, HIERARCHY_NAME_MAX_SIZE)?;
-            HierarchyEntry::Scope {
-                tpe,
-                name,
-                component,
-            }
-        }
-        0..=29 => {
-            // VcdEvent ... SvShortReal (VariableType)
-            let tpe = VarType::try_from_primitive(entry_tpe)?;
-            let direction = VarDirection::try_from_primitive(read_u8(hierarchy)?)?;
-            let name = read_c_str(hierarchy, HIERARCHY_NAME_MAX_SIZE)?;
-            let raw_length = read_variant_32(hierarchy)?;
-            let length = if tpe == VarType::Port {
-                // remove delimiting spaces and adjust signal size
-                (raw_length - 2) / 3
-            } else {
-                raw_length
-            };
-            let alias = read_variant_32(hierarchy)?;
-            let (is_alias, handle) = if alias == 0 {
-                (false, Handle(0)) // TODO: create unique handle by counting!
-            } else {
-                (true, Handle(alias))
-            };
-            HierarchyEntry::Var {
-                tpe,
-                direction,
-                name,
-                length,
-                handle,
-                is_alias,
-            }
-        }
-        255 => {
-            // VcdUpScope (ScopeType)
-            HierarchyEntry::UpScope
-        }
-        252 => {
-            // GenAttributeBegin (ScopeType)
-            todo!("Deal with Attribute Begin entry!")
-        }
-        253 => {
-            // GenAttributeEnd (ScopeType)
-            HierarchyEntry::AttributeEnd
-        }
+impl<R: Read> HierarchyReader<R> {
+    fn new(input: R) -> Self {
+        HierarchyReader { input }
+    }
 
-        other => todo!("Deal with hierarchy entry of type: {other}"),
-    };
+    fn read_all_entries(&mut self) -> Result<()> {
+        loop {
+            let entry = match self.read_entry()? {
+                Some(e) => e,
+                None => return Ok(()),
+            };
+            println!("{entry:?}");
+        }
+    }
 
-    println!("{entry:?}");
-    Ok(entry)
+    fn read_entry(&mut self) -> Result<Option<HierarchyEntry>> {
+        let entry_tpe = match read_u8(&mut self.input) {
+            Ok(tpe) => tpe,
+            Err(e) => return Ok(None),
+        };
+        let entry = match entry_tpe {
+            254 => {
+                // VcdScope (ScopeType)
+                let tpe = ScopeType::try_from_primitive(read_u8(&mut self.input)?)?;
+                let name = read_c_str(&mut self.input, HIERARCHY_NAME_MAX_SIZE)?;
+                let component = read_c_str(&mut self.input, HIERARCHY_NAME_MAX_SIZE)?;
+                HierarchyEntry::Scope {
+                    tpe,
+                    name,
+                    component,
+                }
+            }
+            0..=29 => {
+                // VcdEvent ... SvShortReal (VariableType)
+                let tpe = VarType::try_from_primitive(entry_tpe)?;
+                let direction = VarDirection::try_from_primitive(read_u8(&mut self.input)?)?;
+                let name = read_c_str(&mut self.input, HIERARCHY_NAME_MAX_SIZE)?;
+                let raw_length = read_variant_32(&mut self.input)?;
+                let length = if tpe == VarType::Port {
+                    // remove delimiting spaces and adjust signal size
+                    (raw_length - 2) / 3
+                } else {
+                    raw_length
+                };
+                let alias = read_variant_32(&mut self.input)?;
+                let (is_alias, handle) = if alias == 0 {
+                    (false, Handle(0)) // TODO: create unique handle by counting!
+                } else {
+                    (true, Handle(alias))
+                };
+                HierarchyEntry::Var {
+                    tpe,
+                    direction,
+                    name,
+                    length,
+                    handle,
+                    is_alias,
+                }
+            }
+            255 => {
+                // VcdUpScope (ScopeType)
+                HierarchyEntry::UpScope
+            }
+            252 => {
+                // GenAttributeBegin (ScopeType)
+                todo!("Deal with Attribute Begin entry!")
+            }
+            253 => {
+                // GenAttributeEnd (ScopeType)
+                HierarchyEntry::AttributeEnd
+            }
+
+            other => todo!("Deal with hierarchy entry of type: {other}"),
+        };
+
+        Ok(Some(entry))
+    }
 }
 
-impl<R: Read + Seek> Reader<R> {
+impl<R: Read + Seek> HeaderReader<R> {
     fn new(input: R) -> Self {
-        Reader {
+        HeaderReader {
             input,
             header: None,
             buf: [0u8; 128],
@@ -538,10 +561,8 @@ impl<R: Read + Seek> Reader<R> {
     fn read_hierarchy(&mut self, compression: HierarchyCompression) -> Result<()> {
         // similar to fstReaderIterateHier
         let bytes = self.read_hierarchy_bytes(compression)?;
-        let mut hierarchy = bytes.as_slice();
-        while !hierarchy.is_empty() {
-            read_hierarchy_entry(&mut hierarchy)?;
-        }
+        let mut reader = HierarchyReader::new(bytes.as_slice());
+        reader.read_all_entries()?;
 
         Ok(())
     }
@@ -582,6 +603,6 @@ mod tests {
     fn simple_fst() {
         let f =
             std::fs::File::open("fsts/VerilatorBasicTests_Anon.fst").expect("failed to open file!");
-        Reader::new(f).read().expect("It should work!");
+        HeaderReader::new(f).read().expect("It should work!");
     }
 }

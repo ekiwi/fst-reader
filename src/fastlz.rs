61d2ee55f14f0a8d1a4607120be09fdf97ce4b16
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-10-17 11:45:33 -0400

    fastlz level 2 support

--- src/fastlz.rs
+++ src/fastlz.rs
@@ -17,13 +17,12 @@ pub(crate) fn decompress(
 
     let header = read_u8(input)?;
     let level = (header >> 5) + 1;
+    // go back to header which is actually the first op code!
+    input.seek(SeekFrom::Current(-1))?;
+
     match level {
-        1 => {
-            // go back to header which is actually the first op code!
-            input.seek(SeekFrom::Current(-1))?;
-            decompress_level1(input, input_len - 1, &mut out)?
-        }
-        2 => todo!("implement level2 support"),
+        1 => decompress_level1(input, input_len, &mut out)?,
+        2 => decompress_level2(input, input_len, &mut out)?,
         other => todo!("Better error handling for invalid fastlz level {other}!"),
     };
     Ok(out)
@@ -35,36 +34,95 @@ fn decompress_level1(input: &mut impl Read, input_len: usize, out: &mut Vec<u8>)
     while read_count < input_len {
         let byte0 = read_u8(input)?;
         read_count += 1;
-        let tpe = byte0 >> 5;
-        if tpe == 0 {
-            // literal run
-            let run_length = (1 + byte0) as usize;
-            let mut bytes = read_bytes(input, run_length)?;
-            read_count += run_length;
-            out.append(&mut bytes);
-        } else if tpe < 7 {
-            // short match
-            let byte1 = read_u8(input)?;
+        // long or short match
+        if byte0 >= 32 {
+            let mut length = (byte0 >> 5) as usize + 2;
+            let offset = 256 * ((byte0 & 0x1f) as usize);
+            let mut start = out.len() - offset - 1;
+            // long run (i.e. type == 7)
+            if length == 7 + 2 {
+                length += read_u8(input)? as usize;
+                read_count += 1;
+            }
+            start -= read_u8(input)? as usize; // offset adjustment
             read_count += 1;
-            let offset = 256 * ((byte0 & 0x1f) as usize) + byte1 as usize;
-            let length = 2 + tpe as usize;
-            copy_match(out, offset, length);
+            copy_match(out, start, length);
         } else {
-            // long match
-            let byte1 = read_u8(input)?;
-            let byte2 = read_u8(input)?;
-            read_count += 2;
-            let offset = 256 * ((byte0 & 0x1f) as usize) + byte2 as usize;
-            let length = 9 + byte1 as usize;
-            copy_match(out, offset, length);
+            literal_run(input, byte0, &mut read_count, out)?;
         }
     }
     Ok(())
 }
 
+const MAX_L2_DISTANCE: usize = 8191;
+
+fn decompress_level2(input: &mut impl Read, input_len: usize, out: &mut Vec<u8>) -> ReadResult<()> {
+    let mut read_count: usize = 0;
+    let mut byte0 = read_u8(input)? & 0x1f; // remove header for first read
+    read_count += 1;
+
+    loop {
+        // long or short match
+        if byte0 >= 32 {
+            let mut length = (byte0 >> 5) as usize + 2;
+            let offset = 256 * ((byte0 & 0x1f) as usize);
+            let mut start = out.len() - offset - 1;
+            // long run (i.e. type == 7)
+            if length == 7 + 2 {
+                // lvl 2: read length until we get to a non 0xff byte
+                loop {
+                    let code = read_u8(input)?;
+                    read_count += 1;
+                    length += code as usize;
+                    if code != 255 {
+                        break;
+                    }
+                }
+            }
+            let offset_code = read_u8(input)?;
+            read_count += 1;
+            start -= offset_code as usize; // offset adjustment
+                                           // lvl 2: match from 16-bit distance
+            if offset_code == 255 && byte0 & 0x1f == 31 {
+                let lvl2_offset_high = (read_u8(input)? as usize) << 8;
+                let lvl2_offset = lvl2_offset_high + read_u8(input)? as usize;
+                read_count += 2;
+                // overwrite start
+                start = out.len() - lvl2_offset - MAX_L2_DISTANCE - 1;
+            }
+            copy_match(out, start, length);
+        } else {
+            literal_run(input, byte0, &mut read_count, out)?;
+        }
+
+        // exit the loop
+        if read_count >= input_len {
+            break;
+        }
+
+        // load next instruction
+        byte0 = read_u8(input)?;
+        read_count += 1;
+    }
+    Ok(())
+}
+
 #[inline]
-fn copy_match(out: &mut Vec<u8>, offset: usize, length: usize) {
-    let start = out.len() - offset - 1;
+fn literal_run(
+    input: &mut impl Read,
+    byte0: u8,
+    read_count: &mut usize,
+    out: &mut Vec<u8>,
+) -> ReadResult<()> {
+    let run_length = (1 + byte0) as usize;
+    let mut bytes = read_bytes(input, run_length)?;
+    *read_count += run_length;
+    out.append(&mut bytes);
+    Ok(())
+}
+
+#[inline]
+fn copy_match(out: &mut Vec<u8>, start: usize, length: usize) {
     for ii in start..start + length {
         out.push(out[ii]);
     }
